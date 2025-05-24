7bf72e98b108176cd21d6747fb5ae221b7261869
JRP-8561 <procopio_juliana@yahoo.com.br>
2025-05-24 11:53:00 +0000

    Criando um tabuleiro virtual

--- xadrez.c
+++ xadrez.c
@@ -2,23 +2,38 @@
 
 #define TAMANHO_TABULEIRO 8
 
-// Função para imprimir os índices das colunas
+// Função para imprimir os índices das colunas com espaçamento adequado
 void imprimirIndicesColunas() {
-    printf("   ");
+    printf("     "); // Espaço inicial para alinhar com os índices das linhas
     for (char letra = 'A'; letra < 'A' + TAMANHO_TABULEIRO; letra++) {
-        printf(" %c  ", letra);
+        printf(" %c   ", letra); // Espaços extras para alinhar ao tabuleiro
     }
     printf("\n");
 }
 
-// Função para imprimir uma linha do tabuleiro com índice de linha
+// Função para imprimir uma linha do tabuleiro com posicionamento das peças
 void imprimirLinha(int linha) {
-    printf("%2d ", linha + 1);  // Índice da linha
+    printf("%2d   ", linha + 1); // Índice da linha com espaço extra
     for (int j = 0; j < TAMANHO_TABULEIRO; j++) {
-        if ((linha + j) % 2 == 0) {
-            printf("| | ");
+        // Adicionando as peças nas coordenadas solicitadas
+        if (linha == 0 && j == 2) {
+            printf("|B1| ");  // Bispo 1 em C1
+        } else if (linha == 0 && j == 5) {
+            printf("|B2| ");  // Bispo 2 em F1
+        } else if (linha == 0 && j == 4) {
+            printf("|RA| ");  // Rainha em E1
+        } else if (linha == 0 && j == 0) {
+            printf("|T1| ");  // Torre 1 em A1
+        } else if (linha == 0 && j == 7) {
+            printf("|T2| ");  // Torre 2 em H1
+        } else if (linha == 0 && j == 1) {
+            printf("|C1| ");  // Cavalo 1 em B1
+        } else if (linha == 0 && j == 6) {
+            printf("|C2| ");  // Cavalo 2 em G1
+        } else if ((linha + j) % 2 == 0) {
+            printf("|  | ");  // Casa branca
         } else {
-            printf("|X| ");
+            printf("|XX| ");  // Casa preta
         }
     }
     printf("\n");
@@ -26,10 +41,9 @@ void imprimirLinha(int linha) {
 
 // Função para imprimir o tabuleiro completo
 void imprimirTabuleiro() {
-    imprimirIndicesColunas();  // Imprime os índices das colunas
-
+    imprimirIndicesColunas();
     for (int i = 0; i < TAMANHO_TABULEIRO; i++) {
-        imprimirLinha(i);  // Imprime cada linha do tabuleiro
+        imprimirLinha(i);
     }
 }
 
